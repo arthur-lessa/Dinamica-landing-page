99aa7645e2e16f1d767af86c795a6260c5c75749
thurzin <thurlessaf@gmail.com>
2025-02-26 19:48:28 -0300

    WIP: mudar comportamento da nav bar por completo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  public isMenuOpen = false;
+  isMobile: boolean = window.innerWidth < 1100;
+  isMenuOpen: boolean = false;
 
-  toggleMenu(): void {
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.isMobile = (event.target as Window).innerWidth < 1100;
+
+    if (!this.isMobile) {
+      this.isMenuOpen = false;    
+    }
+  }
+
+  toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
