941dfb2a31eba1f454c9b9bb3977f94da2c4a101
thurzin <thurlessaf@gmail.com>
2025-03-02 11:22:44 -0300

    WIP: ajustes finais sessões sobre e início :))

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -13,20 +13,22 @@ export class AboutComponent {
   viewPort: number = 0;
   isMobile: boolean = false;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
     this.viewPort = window.innerWidth;
     this.isMobile = this.viewPort < 1100;
   }
-  
-  public openDialog(): void {    
-    const dialogWidth = this.isMobile ? '60%' : '1000px';  
+
+  public openDialog(): void {
+    const dialogWidth = this.isMobile ? '60%' : '1000px';
     const dialogHeight = this.isMobile ? '550px' : 'auto';
 
-  this.dialog.open(ImagePopupComponent, {
-    minWidth: dialogWidth,  
-    height: dialogHeight,
-  });
+    this.dialog.open(ImagePopupComponent, {
+      minWidth: dialogWidth,
+      height: dialogHeight,
+      enterAnimationDuration: '500ms',
+      exitAnimationDuration: '500ms',
+    });
   }
 }
