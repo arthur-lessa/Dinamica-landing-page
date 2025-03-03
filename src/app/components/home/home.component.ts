5a9eb6fc99222e01995670fd1f0b0d4c9d7a3a08
thurzin <thurlessaf@gmail.com>
2025-03-02 22:28:00 -0300

    WIP: iniciar cards de serviços

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
 import { AboutComponent } from '../about/about.component';
+import { ServicesComponent } from '../services/services.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     NavbarComponent, 
     WelcomeSectionComponent, 
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
---
+++ src/app/components/services/services.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-services',
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.scss'
+})
+export class ServicesComponent {
+
+}
